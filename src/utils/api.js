c4fccf301b05707fb8a8867b1114427f1ac1fceb
Mukul29RTU <Mukulgarg573@gmail.com>
2026-03-24 02:34:09 +0530

    Fixed Backend Headers and API

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,6 @@ const api = axios.create({
  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': '69420',
   }
 });
 
